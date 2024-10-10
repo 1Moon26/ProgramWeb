c7b5aa3f68d7e321542858d26cb17f8236f6ab79
1Moon26 <mmendezl2201@alumno.ipn.mx>
2024-10-10 16:00:48 -0600

    Clase 10/10/2024

---
+++ Clase_26092024/Script.js
@@ -0,0 +1,20 @@
+function appendToDisplay(value) {
+    document.getElementsById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deletelast() {
+    let displayValue = document.getElementById('display').value;
+    document.getElementById('display').value = displayValue.slice(0, 1);
+}
+
+function calculateResult(){
+    try{
+        let result = eval(document.getElementById('display').value);
+    } catch (error) {
+        alert('Espresion inválida')
+    }
+}
