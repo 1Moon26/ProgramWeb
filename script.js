ee330bb584b1760ad2653f39cdfe92bf6c8844d6
1Moon26 <mmendezl2201@alumno.ipn.mx>
2024-10-10 15:53:25 -0600

    Programación web

---
+++ script.js
@@ -0,0 +1,23 @@
+//CLASE 24/09/24
+
+
+/* LAS VARIABLES GUARDAN INFORMACIÓN QUE PUEDES REUTILIZAR */
+
+//DECLARACIÓN DE VARIABLES 
+let nombre = 'Pedro'; // usa let declara variables que pueden cambiar su valor
+const PI = 3.1416;
+var edad = 25;
+
+//operadores
+let a = 5;
+let b = 2;
+let suma = a + b; //7
+let resta = a - b; //3
+let multiplicacion = a * b; //10
+let división = a / b; //2.5
+
+//operadores de comparación
+a > b; //true
+a < b; //false
+a == b; //false
+a === b; //false (compara valor y tipo)
