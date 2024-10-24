e8cb6f838eabf476e61fd8310bce34eeafd5174e
1Moon26 <mmendezl2201@alumno.ipn.mx>
2024-10-24 16:37:20 -0600

    Clase 24/10/2024

---
+++ 24102024/script.js
@@ -0,0 +1,34 @@
+//Arreglos
+
+/*
+let array = ["Dato 1",1,Boolean];
+console.log("tamaño array " + array.length);
+array.push("Inserto nuevo valor");
+console.log(array[3]);
+console.log("tamaño array " + array.length);
+console.log(array[3]);
+*/
+
+//Ejercicio de la tabla de multiplicar
+/*
+let array = [];
+for (let i=0; i<10 ; i++){
+    
+    array.push( 5 * (i+1)) ;
+    console.log("5 x" + (i+1)  + "= " + array[i]);
+
+    if(i==5){
+        i = 10;
+        console.log("tu arreglo es de " + array.length )
+    }
+}
+let array = ["dato1", "dato2", "dato3"];
+array.push("dato 4");
+console.log(array[3]);
+
+//eliminar
+array.splice(2,2);
+console.log(array)
+*/
+
+
